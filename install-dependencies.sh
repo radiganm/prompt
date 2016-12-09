ad11d915b77bb03144e317f041a16e700ab9eefb
mac <mac@radigan.org>
2016-12-09 02:24:34 -0800

    initial import

---
+++ install-dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## install-dependencies.sh
+## Mac Radigan
+##
+## Copyright 2016 Mac Radigan
+## All Rights Reserved
+
+  cpan Term::ReadLine
+  cpan IPC::Open3;
+  cpan IO::Select;
+
+## *EOF*
